544c51af451b57396e2abf98eaaaf57b9235ef5a
youwinatural <youwinatural@gmail.com>
2019-05-18 15:59:55 +0900

    cppフォルダ内の不要なincludeを削除し hフォルダを作成

--- test.cpp
+++ test.cpp
@@ -1,13 +1,8 @@
-﻿#ifndef _TEST_H_
-#define _TEST_H_
-
-
-#include<stdio.h>
-
+﻿#include<stdio.h>
+#include"test.h"
 void testfunction()
 {
 	printf("testtest");
 
 	//吉村佑惟
 }
-#endif //_TEST_H_
---
+++ test.h
@@ -0,0 +1,11 @@
+#ifndef _TEST_H_
+#define _TEST_H_
+class test
+{
+private:
+
+public:
+	testfunction();
+
+};
+#endif //_TEST_H_
